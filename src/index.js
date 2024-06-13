dc8abfcf00cabd61af2352c768751787fe145f75
Gautam <gautamnjaisur@gmail.com>
2024-06-14 03:18:52 +0530

    commiting the additional changes

--- src/index.js
+++ src/index.js
@@ -2,4 +2,12 @@ import { DB_NAME } from "./constants.js";
 
 import connectDB from "./db/index.js";
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.liste(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at ${process.env.PORT}`);
+    })
+})
+.catch((e)=>{
+    console.log("MONGODB CONNECTION FAILED :",e);
+})
