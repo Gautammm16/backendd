e538e665df0f24d8b044248fae26939a6b2a361e
Gautam <gautamnjaisur@gmail.com>
2024-06-16 00:02:18 +0530

    commit

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -87,14 +87,12 @@ userSchema.methods.generateAccessToken =  function(){
 userSchema.methods.generateRefreshToken =  function(){
     return jwt.sign({
         _id:this.id,
-      
     },
     process.env.REFRESH_TOKEN_SECRET,{
         expiresIn:process.env.REFRESH_TOKEN_EXPIRY
     }
 )
-
 }
-
+ 
 export const User = mongoose.model("User",userSchema);
 
